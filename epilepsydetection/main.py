48726cb48c749e92ad7b7d523fc83048698dd477
THaar50 <haar.tobias@gmail.com>
2025-02-18 16:23:42 +0100

    change data loading to relative path

--- epilepsydetection/main.py
+++ epilepsydetection/main.py
@@ -1,9 +1,8 @@
 import nibabel as nib
 import numpy as np
 import pyvista as pv
-# from app.main import create_app
+from app.main import create_app
 from scipy.ndimage import zoom
-import os
 
 # app = create_app()
 
@@ -11,11 +10,11 @@ SAMPLE_NUMBER = 1
 
 def get_3d_nifti_data(sample_number: int, type: str) -> np.ndarray:
     if type == "mask":
-        mask_file_path = rf"C:\Users\haar\Documents\AI_Consulting\Hackathon\team_10\masks\{sample_number}\{sample_number}_MaskInRawData.nii.gz"
+        mask_file_path = rf"masks\{sample_number}\{sample_number}_MaskInOrig.nii.gz"
         nifti_img = nib.load(mask_file_path)
         return nifti_img.get_fdata() # get the data as a 3D numpy array
     elif type == "raw":
-        raw_file_path = rf"C:\Users\haar\Documents\AI_Consulting\Hackathon\team_10\ds005602-1.0.0\sub-{sample_number}\anat\sub-{sample_number}_T1w.nii.gz"
+        raw_file_path = rf"freesurfer_orig\{sample_number}_freesurfer_orig.nii.gz"
         nifti_img = nib.load(raw_file_path)
         return nifti_img.get_fdata() # get the data as a 3D numpy array
 
@@ -24,8 +23,8 @@ mask_data = get_3d_nifti_data(SAMPLE_NUMBER, "mask")
 
 print(f"Shape of the raw data: {raw_data.shape}")
 print(f"Shape of the mask data: {mask_data.shape}")
-# raw_data[:, :, 133:] = 0 # adjust slice to be plotted
-nifti_data = raw_data + mask_data*4000
+raw_data[:, :, 133:] = 0 # adjust slice to be plotted
+nifti_data = raw_data + mask_data*200
 print(f"Shape of the 3D numpy array: {nifti_data.shape}")
 
 grid = pv.ImageData()
